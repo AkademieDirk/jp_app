15fc282914beb917602941c4efb9e3284357b5db
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-14 18:03:07 +0200

    Carg widget extracte to file

--- lib/Screens/start_screen.dart
+++ lib/Screens/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jp_app/cardstartscreen.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -6,53 +7,40 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Stack(children: [
-        Center(
-            child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                      "assets/assets1/hintergruende/bg_startscreen.png"),
-                  fit: BoxFit.cover)),
-        )),
-        Positioned(
-            top: 120,
-            right: -130,
-            child:
-                Image.asset("assets/assets1/grafiken/chick cupcakes_3D.png")),
-        Positioned(
-          top: 450,
-          left: 50,
-          child: Image.asset(
-            "assets/assets1/details/T2.png",
-            color: Colors.white30,
-          ),
-        ),
-        SafeArea(
-          child: Center(
-            child: Card(
-              color: Colors.transparent,
-              margin: const EdgeInsetsDirectional.only(top: 500, bottom: 100),
-              child: Column(children: [
-                const Text(
-                  "Feeling Snackish Today?",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                ),
-                const Text("Test Text ",
-                    style: TextStyle(color: Colors.white, fontSize: 16)),
-                ElevatedButton(
-                    onPressed: () {}, child: const Text("Order Now")),
-              ]),
+      home: Scaffold(
+        body: Stack(children: [
+          Center(
+              child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                        "assets/assets1/hintergruende/bg_startscreen.png"),
+                    fit: BoxFit.cover)),
+          )),
+          Positioned(
+              top: 120,
+              right: -130,
+              child:
+                  Image.asset("assets/assets1/grafiken/chick cupcakes_3D.png")),
+          Positioned(
+            top: 450,
+            left: 50,
+            child: Image.asset(
+              "assets/assets1/details/T2.png",
+              color: Colors.white30,
             ),
           ),
-        ),
-      ]),
-    ));
+          const Center(
+            child: SizedBox(
+              height: 800,
+              width: 400,
+              child: Cardstartscreen(),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
