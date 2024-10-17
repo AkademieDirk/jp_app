04aeb6fd01df9254cbfcf357b591dc4c20707ea3
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-17 13:45:10 +0200

    Repository Food Objects created

---
+++ lib/Repository/food_objects.dart
@@ -0,0 +1,17 @@
+class FoodObject {
+  final String description;
+  final double price;
+  final double stars;
+  final String title;
+  final String imagePath;
+  final double points;
+
+  FoodObject({
+    required this.points,
+    required this.description,
+    required this.price,
+    required this.imagePath,
+    required this.stars,
+    required this.title,
+  });
+}
---
+++ lib/Repository/food_objects_data.dart
@@ -0,0 +1,46 @@
+import 'package:jp_app/Repository/food_objects.dart';
+
+List<FoodObject> foodobjects = [
+  FoodObject(
+      imagePath: "assets/images/cat cupcakes_3D.png",
+      title: "Mogli's Cup",
+      description: "Cat's are delicious",
+      price: 24.99,
+      points: 300,
+      stars: 3.2),
+  FoodObject(
+      imagePath: "assets/images/Ice.cream.png",
+      title: "Balu's Cup",
+      description: "Pistachio Icecream",
+      price: 10.99,
+      points: 150,
+      stars: 4.4),
+  FoodObject(
+      imagePath: "assets/images/chick cupcakes_3D.png",
+      title: "Birdy Cup",
+      description: "Sweet Chicken",
+      price: 19.99,
+      points: 300,
+      stars: 3.2),
+  FoodObject(
+      imagePath: "assets/images/ice cream stick_3D.png",
+      title: "Sticky Icecream",
+      description: "on Stick with Choclate",
+      price: 9.99,
+      points: 100,
+      stars: 4.2),
+  FoodObject(
+      imagePath: "assets/images/Icecream_3D.png",
+      title: "Vanilla Waffle",
+      description: "Vanilla on Waffle",
+      price: 8.99,
+      points: 150,
+      stars: 3.6),
+  FoodObject(
+      imagePath: "assets/images/Burger_3D.png",
+      title: "Angi's Burger",
+      description: "Delicious vegan Burger",
+      price: 13.99,
+      points: 400,
+      stars: 4.8),
+];
